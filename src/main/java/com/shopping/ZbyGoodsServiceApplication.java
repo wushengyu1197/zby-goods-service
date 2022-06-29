021cd03298573fbb9f8c435e40a7b163dd855e4b
zby <2844216106@qq.com>
2022-06-29 14:32:30 +0800

    shopping1.5

--- src/main/java/com/shopping/ZbyGoodsServiceApplication.java
+++ src/main/java/com/shopping/ZbyGoodsServiceApplication.java
@@ -3,14 +3,14 @@ package com.shopping;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 @SpringBootApplication
 //@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @MapperScan(basePackages = {"com.shopping.mapper"})
-public class ZbyUserServiceApplication {
+public class ZbyGoodsServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ZbyUserServiceApplication.class, args);
+        SpringApplication.run(ZbyGoodsServiceApplication.class, args);
 //        		SpringApplication application=new SpringApplication(ZbyUserServiceApplication.class);
 //		String env="qa";
 //
